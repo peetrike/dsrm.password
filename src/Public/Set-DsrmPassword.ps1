97233b250d4052b537d3e26c49b3612894627f82
Peter Wawa <meelisn@outlook.com>
2026-02-16 17:22:34 +0200

    add new functions

---
+++ src/Public/Set-DsrmPassword.ps1
@@ -0,0 +1,35 @@
+﻿function Set-DsrmPassword {
+    # .EXTERNALHELP Module-help.xml
+    [OutputType([bool])]
+    [CmdletBinding(
+        SupportsShouldProcess
+    )]
+    param (
+            [Parameter(Mandatory)]
+            [securestring]
+        $Password,
+            [string]
+            # computer name for witch to change password
+        $ComputerName = $env:COMPUTERNAME,
+            [string]
+            # prefix for user SamAccountName
+        $Prefix = 'DSRM'
+    )
+
+    $UserName = '{0}-{1}' -f $Prefix, $ComputerName
+    $DsrmUser = Get-ADUser -Identity $UserName -ErrorAction Stop
+
+    if ($PSCmdlet.ShouldProcess($DsrmUser, 'Reset password')) {
+        Set-ADAccountPassword -Identity $DsrmUser -NewPassword $Password -Reset -ErrorAction Stop -Confirm:$false
+
+            # get the resulting event
+        Start-Sleep -Seconds 3
+        $query = @(
+            'System[(EventID = 4724) and TimeCreated[timediff(@SystemTime) <= {0}]]' -f 60000
+            'EventData[Data[@Name="TargetUserName"] = "{0}"]' -f $UserName
+        ) -join ' and '
+        $xPathQuery = '*[{0}]' -f $query
+        $EventRecord = Get-WinEvent -LogName Security -FilterXPath $xPathQuery -MaxEvents 1
+        return (0, 4 -contains $EventRecord.Level)
+    }
+}
---
+++ src/Public/Sync-DsrmPassword.ps1
@@ -0,0 +1,32 @@
+﻿function Sync-DsrmPassword {
+    # .EXTERNALHELP Module-help.xml
+    [OutputType([bool])]
+    [CmdletBinding(
+        SupportsShouldProcess
+    )]
+    param (
+            [string]
+            # computer name for witch to change password
+        $ComputerName = $env:COMPUTERNAME,
+            [string]
+            # prefix for user SamAccountName
+        $Prefix = 'DSRM'
+    )
+
+    $UserName = '{0}-{1}' -f $Prefix, $ComputerName
+    if ($PSCmdlet.ShouldProcess($ComputerName, 'Sync DSRM Password')) {
+        $result = ntdsutil.exe 'set dsrm password' "sync from domain account $UserName" q q
+        Write-Verbose -Message ("{0}`n{1}" -f $result[1], $result[2])
+
+            # get the resulting event
+        Start-Sleep -Seconds 3
+        $query = @(
+            'System[(EventID = 4794) and TimeCreated[timediff(@SystemTime) <= {0}]]' -f 60000
+            'EventData[Data[@Name="Workstation"] = "{0}"]' -f $ComputerName
+        ) -join ' and '
+        $xPathQuery = '*[{0}]' -f $query
+        $EventRecord = Get-WinEvent -LogName Security -FilterXPath $xPathQuery -MaxEvents 1
+        $XmlEvent = [xml] $EventRecord.ToXml()
+        return $XmlEvent.SelectSingleNode('//*[@Name="Status"]').InnerText -eq '0x0'
+    }
+}
