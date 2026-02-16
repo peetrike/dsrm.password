00056411e4515610c32d92e1d8d5feac69df72ef
Peter Wawa <meelisn@outlook.com>
2026-02-16 20:29:04 +0200

    Remove ComputerName from parameters
    this function synchronizes local computer DSRM password.  So the parameter has no usage.

--- src/Public/Sync-DsrmPassword.ps1
+++ src/Public/Sync-DsrmPassword.ps1
@@ -6,15 +6,11 @@
     )]
     param (
             [string]
-            # computer name for witch to change password
-        $ComputerName = $env:COMPUTERNAME,
-            [string]
-            # prefix for user SamAccountName
         $Prefix = 'DSRM'
     )
 
-    $UserName = '{0}-{1}' -f $Prefix, $ComputerName
-    if ($PSCmdlet.ShouldProcess($ComputerName, 'Sync DSRM Password')) {
+    $UserName = '{0}-{1}' -f $Prefix, $env:COMPUTERNAME
+    if ($PSCmdlet.ShouldProcess($env:COMPUTERNAME, 'Sync DSRM Password')) {
         $result = ntdsutil.exe 'set dsrm password' "sync from domain account $UserName" q q
         Write-Verbose -Message ("{0}`n{1}" -f $result[1], $result[2])
 
@@ -22,7 +18,7 @@
         Start-Sleep -Seconds 3
         $query = @(
             'System[(EventID = 4794) and TimeCreated[timediff(@SystemTime) <= {0}]]' -f 60000
-            'EventData[Data[@Name="Workstation"] = "{0}"]' -f $ComputerName
+            'EventData[Data[@Name="Workstation"] = "{0}"]' -f $env:COMPUTERNAME
         ) -join ' and '
         $xPathQuery = '*[{0}]' -f $query
         $EventRecord = Get-WinEvent -LogName Security -FilterXPath $xPathQuery -MaxEvents 1
